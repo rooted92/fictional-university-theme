d8e9b45680c9f9730c7d88e40a27129b12d036a7
Pedro Castaneda <petecast76@gmail.com>
2023-05-30 12:17:22 -0700

    used built in wp func(s) to make site dynamic

--- page.php
+++ page.php
@@ -10,9 +10,12 @@ while (have_posts()) {
         <h2><?php the_title(); ?></h2>
         <?php the_content(); ?> -->
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__bg-image"
+            style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <h1 class="page-banner__title">
+                <?php the_title(); ?>
+            </h1>
             <div class="page-banner__intro">
                 <p>DONT FORGET TO REPLACE ME LATER</p>
             </div>
@@ -20,12 +23,22 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About
-                    Us</a> <span class="metabox__main">Our History</span>
-            </p>
-        </div>
+        <?php
+        // example of using post ID's in wp built in functions to get parent id from child page.
+        // pass in id of our goals page (using get the id func) into get parent id func and it will return the id of that childs parent
+        // will return 0 if page does not have parent
+        $the_parent = wp_get_post_parent_id(get_the_ID());
+        if ($the_parent) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($the_parent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($the_parent); ?></a> <span class="metabox__main">
+                        <?php the_title(); ?>
+                    </span>
+                </p>
+            </div>
+        <?php }
+        ?>
+
 
         <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
